9b96c75e5a56236c6eb80761d30f3092340cf489
b1ackd0t <blackd0t@protonmail.com>
2023-01-17 23:44:38 +0300

    add more erros
    Signed-off-by: b1ackd0t <blackd0t@protonmail.com>

--- pkg/errors.go
+++ pkg/errors.go
@@ -3,7 +3,14 @@ package pkg
 import "errors"
 
 var (
-	ErrMissingBindDB      = errors.New("missing binddb parameter")
-	ErrMissingBindPass    = errors.New("missing bindpass parameters")
-	ErrMissingServiceName = errors.New("missing service name acccount")
+	ErrMissingBindDB          = errors.New("missing binddb parameter")
+	ErrMissingBindPass        = errors.New("missing bindpass parameters")
+	ErrMissingServiceName     = errors.New("missing service name acccount")
+	ErrMissingAccessKey       = errors.New("missing access key")
+	ErrMissingSecretKey       = errors.New("missing secret key")
+	ErrMissingName            = errors.New("missing name")
+	ErrMissingLease           = errors.New("missing lease")
+	ErrMissingLeaseMax        = errors.New("missing lease max")
+	ErrMissingCredentialsType = errors.New("missing credentials type")
+	ErrInvalidAWSCredType     = errors.New("invalid aws credentials type it is either sts or creds")
 )
